113ebc82d5fe9ca5d386987b4dd49f938c05bd38
IlMike22 <michaelwidlok@gmail.com>
2024-05-20 08:50:32 +0200

    setup refresh token mechanism

---
+++ core/data/src/main/java/com/example/core/data/networking/AccessTokenResponse.kt
@@ -0,0 +1,10 @@
+package com.example.core.data.networking
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class AccessTokenResponse(
+    val accessToken: String,
+    val expirationTimeStamp: String
+)
+
